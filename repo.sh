03159288c04bb7ef24fd03eacda98555107e123d
vishal-sudo <vishal1.80arya@gmail.com>
2021-11-20 22:22:37 +0530

    parts

--- repo.sh
+++ repo.sh
@@ -11,10 +11,10 @@ ls
 sleep 4s
 repo-add -s -n -R $name.db.tar.gz *.pkg.tar.zst
 
-rm -rf $name.db
-rm -rf $name.files
-mv $name.db.tar.gz $name.db
-mv $name.files.tar.gz $name.files
+#rm -rf $name.db
+#rm -rf $name.files
+#mv $name.db.tar.gz $name.db
+#mv $name.files.tar.gz $name.files
 echo ##################
 echo "$name Repo Added"
 echo ##################
