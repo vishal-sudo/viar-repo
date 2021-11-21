fb3d75804a809cd4bd724559baffd08e8ad9444e
vishal-sudo <vishal1.80arya@gmail.com>
2021-11-21 10:43:14 +0530

    update

--- repo.sh
+++ repo.sh
@@ -1,20 +1,24 @@
 #! /bin/env bash
+
+
 echo ##################
 echo "Started"
 echo ##################
-#read name
-name="viar_os_repo"
+
+name="viar-repo"
+
 cd x86_64/
-rm -rf *.db *.db.tar.gz.old *.files.tar.gz.old *.files.old
-rm -rf *.db *.db.tar.gz *.files.tar.gz *.files
-ls
+rm -rf $name.*
+
 sleep 4s
+
 repo-add -s -n -R $name.db.tar.gz *.pkg.tar.zst
 
 #rm -rf $name.db
 #rm -rf $name.files
-#mv $name.db.tar.gz $name.db
-#mv $name.files.tar.gz $name.files
+mv $name.db.tar.gz $name.db
+mv $name.files.tar.gz $name.files
+
 echo ##################
 echo "$name Repo Added"
 echo ##################
